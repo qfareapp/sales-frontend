55dc095cd739a8b53fad2fd93de3febb9233017d
Pranjal Mullick <me.pranjal@gmail.com>
2025-10-22 16:31:30 +0530

    fix: updated src/api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.MODE === "production"
-      ? "https://sales-backend-covv.onrender.com"
+      ? "https://sales-backend-covv.onrender.com/api"
       : "http://localhost:5000/api",
 });
 
